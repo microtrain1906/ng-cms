6d3008204afa27cc0bf0d8727eed3f275897f037
chad-svastisalee <swasgrafx@gmail.com>
2019-08-07 09:35:05 -0500

    Generated and routed UserCreateComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent }   from './users/users.component';
 import { UserViewComponent }   from './user-view/user-view.component';
+import { UserCreateComponent }   from './user-create/user-create.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
   { path: 'users', component: UsersComponent },
-  { path: 'users/view/:id', component: UserViewComponent }
+  { path: 'users/view/:id', component: UserViewComponent },
+  { path: 'users/create', component: UserCreateComponent }
 ];
 
 @NgModule({
